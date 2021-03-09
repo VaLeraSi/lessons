b971dc2c3541365fdc7edb5b69a9fdbc4a32018a
LeraSidorova <valeralera22@icloud.com>
2021-03-09 14:37:07 +0300

    Added Practice 4

---
+++ utils.py
@@ -0,0 +1,16 @@
+from requests import get, utils
+
+response = get("http://www.cbr.ru/scripts/XML_daily.asp")
+encodings = utils.get_unicode_from_response(response)
+
+
+def currency_rates(currency):
+    content = encodings.split("<Valute ID=")
+    for i in content:
+        if currency in i:
+            return float(i.replace("/", "").split("<Value>")[1].replace(",", "."))
+
+
+if __name__ == "__main__":
+    print(currency_rates("USD"))
+    print(currency_rates("EUR"))
